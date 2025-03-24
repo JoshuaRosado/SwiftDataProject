ca93cce08907f6915f4a51258f9f908baf75a904
JoshuaRosado <joshua06rosado@gmail.com>
2025-03-24 11:18:08 -0500

    EditUserView added

--- SwiftDataProject/ContentView.swift
+++ SwiftDataProject/ContentView.swift
@@ -4,18 +4,43 @@
 //
 //  Created by Joshua Rosado Olivencia on 3/24/25.
 //
-
+import SwiftData
 import SwiftUI
 
 struct ContentView: View {
+    @Environment(\.modelContext) var modelContext
+    @Query(sort: \User.name) var users: [User]
+    
+    // Path == empty array of User
+    @State private var path = [User]()
+    
+    
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+        NavigationStack(path: $path){
+            List(users){ user in
+                NavigationLink(value: user) {
+                    Text(user.name)
+                }
+            }
+            .navigationTitle("Users")
+            .navigationDestination(for: User.self){ user in
+                EditUserView(user: user)
+            }
+            .toolbar{
+                Button("Add User", systemImage: "plus"){
+                    // Our user is a NEW USER
+                    // no name, no city
+                    let user = User(name: "", city: "", joinDate: .now)
+                    // add new USER to our ModelContext
+                    modelContext.insert(user)
+                    //Push new USER into path
+                    path = [user]
+                    
+                    
+                    
+                }
+            }
         }
-        .padding()
     }
 }
 
