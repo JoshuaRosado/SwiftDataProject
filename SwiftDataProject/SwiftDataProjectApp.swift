855184e5022a5e58fbd58714b10720e0a8094adf
JoshuaRosado <joshua06rosado@gmail.com>
2025-03-24 10:03:29 -0500

    Initial Commit

---
+++ SwiftDataProject/SwiftDataProjectApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftDataProjectApp.swift
+//  SwiftDataProject
+//
+//  Created by Joshua Rosado Olivencia on 3/24/25.
+//
+
+import SwiftUI
+
+@main
+struct SwiftDataProjectApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
