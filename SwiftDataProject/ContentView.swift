31b3b902acb060a9dbe14b6d55aa03e1c71af88b
JoshuaRosado <joshua06rosado@gmail.com>
2025-03-25 14:51:33 -0500

    Filtering our Query from our modelContext using #Predicate

--- SwiftDataProject/ContentView.swift
+++ SwiftDataProject/ContentView.swift
@@ -10,35 +10,62 @@ import SwiftUI
 struct ContentView: View {
     @Environment(\.modelContext) var modelContext
     
-    @Query(sort: \User.name) var users: [User]
+    // filtering the #Predicate of User class model
+    // If equation is true, then add user to our model
+    // display each user that contains an "B" in its name
+    @Query(filter: #Predicate<User> { user in
+        user.name.localizedStandardContains("N") &&
+        user.city == "Vega Baja"
+    }, sort: \User.name) var users: [User]
     
-    // Path == empty array of User
-    @State private var path = [User]()
+    
+    // Rewriting this query to be readable!
+    
+//    @Query(filter: #Predicate<User>) { user in {
+//        if user.name.localizedStandardContains("N"){
+//            if user.city == "Vega Baja"{
+//                return true
+//            }
+//            return false
+//        }
+//        return false
+//    }}
+    
+    
+    @Query(filter: #Predicate<User> { user in
+        user.age > 30 && user.city == "Vega Baja"
+    } , sort: \User.age) var users2 : [User]
 
-    
-    
     var body: some View {
-        NavigationStack(path: $path){
-            List(users){ user in
-                NavigationLink(value: user) {
+        NavigationStack{
+            List(users2){ user in
+                
                     Text(user.name)
-                }
+                
             }
             .navigationTitle("Users")
-            .navigationDestination(for: User.self){ user in
-                EditUserView(user: user)
-            }
+            
+            
             .toolbar{
-                Button("Add User", systemImage: "plus"){
-                    //
-                    // Our user is a NEW USER
-                    // no name, no city
-                    let user = User(name: "", city: "", joinDate: .now)
-                    // add new USER to our ModelContext
-                    modelContext.insert(user)
-                    //Push new USER into path
-                    path = [user]
+                Button("Add Sample", systemImage: "plus"){
+                    try? modelContext.delete(model: User.self)
                     
+                    let first = User(name: "Residente", city: "Vega Baja", joinDate: .now.addingTimeInterval(86400 * -10), age: 44)
+                    
+                    let second = User(name: "Bad Bunny", city: "Vega Baja", joinDate: .now.addingTimeInterval(86400 * -5), age: 31)
+                    
+                    let third = User(name: "Kendrick Lamar", city: "Compton ", joinDate: .now.addingTimeInterval(86400 * 5), age: 36)
+                    
+                    let fourth = User(name: "Elvis Presley", city: "Memphis", joinDate: .now.addingTimeInterval(86400 * 10), age: 88)
+                    
+                    let fifth = User(name: "Lamar Odom", city: "LA", joinDate: .now.addingTimeInterval(86400 * 15), age: 46)
+                    
+                    
+                    modelContext.insert(first)
+                    modelContext.insert(second)
+                    modelContext.insert(third)
+                    modelContext.insert(fourth)
+                    modelContext.insert(fifth)
                     
                     
                 }
--- SwiftDataProject/User.swift
+++ SwiftDataProject/User.swift
@@ -14,10 +14,12 @@ class User {
     var name: String
     var city: String
     var joinDate: Date
+    var age : Int
     
-    init(name: String, city: String, joinDate: Date) {
+    init(name: String, city: String, joinDate: Date, age: Int) {
         self.name = name
         self.city = city
         self.joinDate = joinDate
+        self.age = age
     }
 }
